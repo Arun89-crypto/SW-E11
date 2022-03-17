24b4fdd1629abc520a4d38f25810f467fc9412b9
Shashank Kumar <shashankkumarthakur@gmail.com>
2022-03-17 14:49:06 +0530

    ✨ Create signup and login

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,11 +1,16 @@
-import express from './express';
-import Logger from './logger';
-import Express from 'express';
+import express from "./express";
+import Logger from "./logger";
+import Express from "express";
+import { initDbClient } from "./db";
 
-export default async ({ expressApp }: { expressApp: Express.Application }): Promise<void> => {
+export default async ({
+  expressApp,
+}: {
+  expressApp: Express.Application;
+}): Promise<void> => {
+  await initDbClient();
+  express({ app: expressApp });
+  Logger.info("✌️ Express loaded");
 
-    express({ app: expressApp });
-    Logger.info('✌️ Express loaded');
-
-    Logger.info('✅ All modules loaded!');
-}; 
+  Logger.info("✅ All modules loaded!");
+};
